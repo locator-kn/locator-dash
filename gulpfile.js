42575a5db894dee792ea635ccac98de3c858f093
Timo Weiß <info@timo-weiss.com>
2015-09-11 23:07:13 +0200

    init gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+var gulp = require('gulp');
+var template = require('gulp-template');
+
+var fs = require('fs');
+
+
+gulp.task('default', function () {
+
+    var data = fs.readFileSync('./env.json', 'utf-8');
+
+    var parsedData = JSON.parse(data);
+    return gulp.src('./keen.dashboard.js')
+        .pipe(template(parsedData))
+        .dest('./out.js');
+
+});
