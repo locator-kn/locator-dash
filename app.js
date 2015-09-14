cf9fbd5bd027517df3977a6a7d7128cd1f23c0c9
Timo Weiß <info@timo-weiss.com>
2015-09-15 00:51:43 +0200

    each chart will be handled through one directive

--- app.js
+++ app.js
@@ -22,79 +22,101 @@ angular.module('locatordash', ['angular-keenio', 'ui.router'])
                 controllerAs: 'lc'
             })
     })
-    .directive('areaChart', function () {
+    .directive('chart', function () {
         return {
             scope: {
                 method: '@',
                 queryOptions: '=',
                 groupByOptions: '=',
-                chartTitle: '@'
+                chartTitle: '@',
+                type: '@'
             },
             controller: function ($scope, tbkKeen, $timeout) {
-                $scope.days = 7;
-                $scope.showChart = true;
+                tbkKeen.ready(function () {
+
+                    $scope.days = 7;
+                    $scope.showChart = true;
 
 
-                $scope.showOptions = $scope.groupByOptions.length > 1;
+                    $scope.showOptions = $scope.groupByOptions && $scope.groupByOptions.length > 1;
 
-                if ($scope.groupByOptions.length) {
-                    $scope.gb = $scope.groupByOptions[0];
-                }
-
-                $scope.chartOptions = {
-                    chartArea: {
-                        height: "85%",
-                        left: "5%",
-                        top: "5%",
-                        width: "80%"
+                    if ($scope.groupByOptions && $scope.groupByOptions.length) {
+                        $scope.gb = $scope.groupByOptions[0];
                     }
-                };
+                    if ($scope.type === 'area') {
 
-
-                var configuration = {
-                    method: $scope.method,
-                    opts: $scope.queryOptions
-                };
-
-                configuration.opts.timeframe = getTimeframeByDays($scope.days);
-
-
-                $scope.$watch('days', function (ol, ne) {
-                    if (ol !== ne) {
-                        $scope.showChart = false;
-                        configuration.opts.timeframe = getTimeframeByDays($scope.days);
-                        performQuery();
-                        $timeout(function () {
-                            $scope.showChart = true;
-                        }, 500);
+                        $scope.chartOptions = {
+                            chartArea: {
+                                height: "85%",
+                                left: "5%",
+                                top: "5%",
+                                width: "80%"
+                            }
+                        };
+                    } else if ($scope.type === 'pie') {
+                        $scope.chartOptions = {
+                            chartArea: {
+                                height: "85%",
+                                left: "5%",
+                                top: "5%",
+                                width: "100%"
+                            },
+                            pieHole: .4
+                        };
+                    } else if ($scope.type === 'metric') {
+                        $scope.chartOptions = {
+                            title: "Registrations",
+                            colors: ["#49c5b1"]
+                        };
                     }
+
+
+                    var configuration = {
+                        method: $scope.method,
+                        opts: $scope.queryOptions
+                    };
+
+                    configuration.opts.timeframe = getTimeframeByDays($scope.days);
+
+
+                    $scope.$watch('days', function (ol, ne) {
+                        if (ol !== ne) {
+                            $scope.showChart = false;
+                            configuration.opts.timeframe = getTimeframeByDays($scope.days);
+                            performQuery();
+                            $timeout(function () {
+                                $scope.showChart = true;
+                            }, 500);
+                        }
+                    });
+                    if (!$scope.queryOptions || !$scope.method) {
+                        throw new Error('Wrong directive usage');
+                    }
+                    $scope.timeframe = 'this_7_days';
+                    function getTimeframeByDays(num_days) {
+                        return 'this_' + num_days + '_days';
+                    }
+
+                    $scope.updateGroupByOption = function (option) {
+                        if ($scope.gb !== option) {
+                            $scope.showChart = false;
+                            $scope.gb = option;
+                            configuration.opts.groupBy = option;
+                            performQuery();
+                            $timeout(function () {
+                                $scope.showChart = true;
+                            }, 500);
+                        }
+                    };
+
+                    function performQuery() {
+                        $scope.query = new tbkKeen.Query(configuration.method, configuration.opts);
+                    }
+
+                    performQuery();
+                    $scope.$apply();
+
                 });
-                if (!$scope.queryOptions || !$scope.method) {
-                    throw new Error('Wrong directive usage');
-                }
-                $scope.timeframe = 'this_7_days';
-                function getTimeframeByDays(num_days) {
-                    return 'this_' + num_days + '_days';
-                }
-
-                $scope.updateGroupByOption = function (option) {
-                    if ($scope.gb !== option) {
-                        $scope.showChart = false;
-                        $scope.gb = option;
-                        configuration.opts.groupBy = option;
-                        performQuery();
-                        $timeout(function () {
-                            $scope.showChart = true;
-                        }, 500);
-                    }
-                };
-
-                function performQuery() {
-                    $scope.query = new tbkKeen.Query(configuration.method, configuration.opts);
-                }
-                performQuery();
-
-
             },
             templateUrl: 'pages/areaChart.html'
         };
