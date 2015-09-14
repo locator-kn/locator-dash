a2d8039817713095e46df8c406812851a75eddf2
Timo Weiß <info@timo-weiss.com>
2015-09-14 19:02:59 +0200

    init template

--- app.js
+++ app.js
@@ -22,3 +22,57 @@ angular.module('locatordash', ['angular-keenio', 'ui.router'])
                 controllerAs: 'lc'
             })
     })
+    .directive('areaChart', function() {
+        return {
+            scope: {
+                method: '@',
+                eventcollection: '@',
+                groupBy: '@'
+            },
+            controller: function($scope, tbkKeen, $timeout) {
+                $scope.days = 7;
+                $scope.showChart = true;
+
+                $scope.chartOptions = {
+                    chartArea: {
+                        height: "85%",
+                        left: "5%",
+                        top: "5%",
+                        width: "80%"
+                    }
+                };
+
+                $scope.$watch('days', function(ol, ne) {
+                    if(ol !== ne) {
+                        $scope.showChart = false;
+                        $timeout(function() {
+                            $scope.showChart = true;
+                        }, 500)
+                    }
+                });
+
+
+
+                if(!$scope.eventcollection || !$scope.method) {
+                    throw new Error('Wrong directive usage');
+                }
+                $scope.timeframe = 'this_7_days';
+                function getTimeframeByDays(num_days) {
+                    return 'this_' + num_days + '_days'
+                }
+
+                var configuration = {
+                    method: $scope.method,
+                    opts: {
+                        eventCollection: $scope.eventcollection,
+                        timeframe: getTimeframeByDays($scope.days),
+                        groupBy: "ip_geo_info.city"
+                    }
+                };
+
+                $scope.query = new tbkKeen.Query(configuration.method, configuration.opts);
+
+            },
+            templateUrl: 'pages/areaChart.html'
+        };
+    });
